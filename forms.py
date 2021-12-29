971e8dc9e7d9c7800036416a87425251bbfbe3bd
Rajnish <palrajnishpal50@gmail.com>
2021-12-29 15:31:15 +0530

    first commit

---
+++ forms.py
@@ -0,0 +1,21 @@
+class register_form(FlaskForm):
+    username = StringField(
+        validators=[
+            InputRequired(),
+            Length(3, 20, message="Please provide a valid name"),
+            Regexp(
+                "^[A-Za-z][A-Za-z0-9_.]*$",
+                0,
+                "Usernames must have only letters, " "numbers, dots or underscores",
+            ),
+        ]
+    )
+    email = StringField(validators=[InputRequired(), Email(), Length(1, 64)])
+    pwd = PasswordField(validators=[InputRequired(), Length(8, 72)])
+    cpwd = PasswordField(
+        validators=[
+            InputRequired(),
+            Length(8, 72),
+            EqualTo("pwd", message="Passwords must match !"),
+        ]
+    )
